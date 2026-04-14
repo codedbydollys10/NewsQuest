37a3efea8f13c17aff8a7a3f4d745fe756e9153e
Komal2008 <realkomalpandey708139@gmail.com>
2026-04-14 18:03:09 +0530

    remove backend and frontend

--- backend/scripts/runtime.mjs
+++
@@ -1,9 +0,0 @@
-import { Blob, File } from 'node:buffer';
-
-if (typeof globalThis.Blob === 'undefined') {
-  globalThis.Blob = Blob;
-}
-
-if (typeof globalThis.File === 'undefined') {
-  globalThis.File = File;
-}
